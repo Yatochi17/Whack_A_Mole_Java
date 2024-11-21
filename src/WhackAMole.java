c2f327eea82811055389615d3c4c12a276d165cf
Yatochi <aqildanial.17@gmail.com>
2024-11-21 21:48:42 +0800

    setup frame

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,3 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.random.*;
+
+
+
 public class WhackAMole {
 
+    int boardWith = 600;
+    int boardHeight = 650;
+
+    JFrame frame = new JFrame("Whack A Mole");
+
+    WhackAMole() {
+        frame.setVisible(true);
+        frame.setSize(boardWith, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
